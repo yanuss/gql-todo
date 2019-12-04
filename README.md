d6c7ef1a0a3d0ae05958f65eebb1529eaedb9ee1
Pawel Janus <paweljj@gmail.com>
2019-12-04 01:53:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# gql-todo
+
+## STACK
+### Frontend:
+  * React
+  * Apollo Client
+### Backend:
+  - GraphQL Yoga
+  - Prisma
+
+
+### scripts
+
+```js
+yarn dev
+```
+to start frontend and backend
---
+++ backend/src/resolvers/Mutation.js
@@ -0,0 +1,8 @@
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+    return item;
+  }
+};
+
+module.exports = Mutation;
