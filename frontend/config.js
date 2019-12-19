41da2943f94102346910f169571a2f716d37cf08
Pawel Janus <paweljj@gmail.com>
2019-12-19 15:17:51 +0100

    prod url

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 export const endpoint = "http://localhost:4444";
-export const prodEndpoint = "http://localhost:4444";
+export const prodEndpoint = "https://gql-todo-yoga-prod.herokuapp.com/";
 export const fbAppId = "789519518157172";
 export const googleAppId =
   "1043638618323-h0msq7ktvro0mp1tgegeqo4lq08e294b.apps.googleusercontent.com";
