cc825888fbb74a55eec7bef3306cc4ba9d4c4101
Pawel Janus <paweljj@gmail.com>
2019-12-24 13:22:19 +0100

    update

--- frontend/components/Meta/Meta.tsx
+++ frontend/components/Meta/Meta.tsx
@@ -1,11 +1,12 @@
+import React from "react";
 import Head from "next/head";
 
 const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
-    <link rel="shortcut icon" href="/public/favicon.png" />
-    <link rel="stylesheet" type="text/css" href="/public/nprogress.css" />
+    {/* <link rel="shortcut icon" href="/public/favicon.ico" /> */}
+    {/* <link rel="stylesheet" type="text/css" href="/public/nprogress.css" /> */}
     <title>Todo</title>
   </Head>
 );
