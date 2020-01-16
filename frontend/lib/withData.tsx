3e5736fac73c8fb4a73a5e13c7b9a54bab46ea1b
Pawel Janus <paweljj@gmail.com>
2020-01-16 16:10:48 +0100

    var

--- frontend/lib/withData.tsx
+++ frontend/lib/withData.tsx
@@ -7,7 +7,7 @@ function createClient({ headers }: { headers?: any }) {
     uri:
       process.env.NODE_ENV === "development"
         ? endpoint
-        : process.env.PROVIDER === "heroku"
+        : process.env.DOMAIN === "heroku"
         ? prodEndpointHeroku
         : prodEndpointNow,
     request: operation => {
